e5af9516ee342698ae64bf0e0936ac7a4f6a147d
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-03-20 09:37:24 +0300

    mainCity: edit html in-place

--- js/mainCity.js
+++ js/mainCity.js
@@ -1,38 +1,15 @@
 const defaultCityName = 'Helsinki';
 
-function createMainCityHtml(cityInfo) {
-  return `<div id="main-city-minimal-info">
-            <h3 id="main-city-name">${cityInfo.name}</h3>
-            <div class="weather-icon-temp">
-                <img class="weather-icon"
-                     id="main-weather-icon"
-                     src="${cityInfo.iconSrc}"
-                     alt="Weather icon">
-                <span class="temperature" id="main-city-temperature">${cityInfo.temperature}&#176;C</span>
-            </div>
-        </div>
-        <ul class="extended-info">
-            <li class="extended-info-li">
-                <span class="info-type">Wind</span>
-                <span class="info-value">${cityInfo.windSpeed} m/s, ${cityInfo.windDirection}</span>
-            </li>
-            <li class="extended-info-li">
-                <span class="info-type">Cloudiness</span>
-                <span class="info-value">${cityInfo.cloudiness}</span>
-            </li>
-            <li class="extended-info-li">
-                <span class="info-type">Pressure</span>
-                <span class="info-value">${cityInfo.pressure}hPa</span>
-            </li>
-            <li class="extended-info-li">
-                <span class="info-type">Humidity</span>
-                <span class="info-value">${cityInfo.humidity}%</span>
-            </li>
-            <li class="extended-info-li">
-                <span class="info-type">Location</span>
-                <span class="info-value">[${cityInfo.locationLat}, ${cityInfo.locationLon}]</span>
-            </li>
-        </ul>`;
+function updateMainCityHtml(cityInfo) {
+  const getElem = (elem) => document.getElementById(elem);
+  getElem('main-city-name').innerHTML = cityInfo.name;
+  getElem('main-weather-icon').src = cityInfo.iconSrc;
+  getElem('main-city-temperature').innerHTML = `${cityInfo.temperature}&#176;C`;
+  getElem('main-city-wind').innerHTML = `${cityInfo.windSpeed} m/s, ${cityInfo.windDirection}`;
+  getElem('main-city-clouds').innerHTML = cityInfo.cloudiness;
+  getElem('main-city-pressure').innerHTML = `${cityInfo.pressure}hPa`;
+  getElem('main-city-humidity').innerHTML = `${cityInfo.humidity}%`;
+  getElem('main-city-loc').innerHTML = `[${cityInfo.locationLat}, ${cityInfo.locationLon}]`;
 }
 
 function mainCityFillFromCurrent(pos) {
@@ -40,8 +17,7 @@ function mainCityFillFromCurrent(pos) {
   const lon = pos.coords.longitude;
 
   let onSuccess = (response) => {
-    document.getElementById('main-city').innerHTML = createMainCityHtml(
-        CityInfo.buildFromResponse(response));
+    updateMainCityHtml(CityInfo.buildFromResponse(response));
   };
 
   let onFail = () => {
@@ -55,23 +31,19 @@ function mainCityFillFromDefault() {
   alert("Location retrieval error, using default");
 
   let onSuccess = (response) => {
-    document.getElementById('main-city').innerHTML = createMainCityHtml(
-        CityInfo.buildFromResponse(response));
+    updateMainCityHtml(CityInfo.buildFromResponse(response));
   };
 
   let onFail = () => {
     alert("Fatal error, leaving main city empty");
-    document.getElementById('main-city').innerHTML = createMainCityHtml(
-        CityInfo.buildEmpty(defaultCityName));
+    updateMainCityHtml(CityInfo.buildEmpty(defaultCityName));
   };
 
   safeRequestWeatherInfoFromName(defaultCityName, onSuccess, onFail);
 }
 
 function refreshLocationListener() {
-  document.getElementById('main-city').innerHTML = createMainCityHtml(
-      CityInfo.buildEmpty(defaultCityName));
-
+  updateMainCityHtml(CityInfo.buildEmpty(defaultCityName));
   navigator.geolocation.getCurrentPosition(mainCityFillFromCurrent,
       mainCityFillFromDefault);
 }
